89387235d6073374d5d0bb7a17394d5d0b6ef75f
Norbert Tóth <ntoth1990@gmail.com>
2019-07-19 15:06:01 +0200

    pictures added

--- js/got.js
+++ js/got.js
@@ -3,6 +3,16 @@ const GameOfThrones = {
   init() {
     this.getJson();
   },
+  orederByName() {
+    this.aliveCharachters.sort((a, b) => a.name.localeCompare(b.name));
+  },
+  createMap() {
+    let str = '';
+    for (let i = 0; i<this.aliveCharachters.length; i += 1) {
+      str += `<div><img src="/${this.aliveCharachters[i].portrait}"><br>${this.aliveCharachters[i].name}</div>`
+    }
+    document.querySelector('.map').innerHTML = str;
+  },
   getAliveCharachters() {
     this.aliveCharachters = [];
     for (let i = 0; i < this.charachters.length; i += 1) {
@@ -10,11 +20,12 @@ const GameOfThrones = {
         this.aliveCharachters.push(this.charachters[i]);
       }
     }
-    console.log(this.aliveCharachters);
+    this.orederByName();
   },
   getData(jsonContent) {
     this.charachters = JSON.parse(jsonContent);
     this.getAliveCharachters();
+    this.createMap();
   },
   getJson() {
     const request = new XMLHttpRequest();
