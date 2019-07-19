7942ee309811f184d5067b79b29bc0455a96224e
Norbert Tóth <ntoth1990@gmail.com>
2019-07-19 13:45:04 +0200

    get charachters added

--- js/got.js
+++ js/got.js
@@ -1,21 +1,19 @@
-var userDatas;
-
-function getData(url, callbackFunc) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function getCharacters() {
-    if (this.readyState === 4 && this.status === 200) {
-      callbackFunc(this);
-    }
-  };
-  xhttp.open('GET', url, true);
-  xhttp.send();
-}
-
-function successAjax(xhttp) {
-  userDatas = JSON.parse(xhttp.responseText);
-}
-
-getData('/json/aJsonFileodNeve.json', successAjax);
-
-// Live servert használd mindig!!!!!
-/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+const GameOfThrones = {
+  init() {
+    const charachters = [];
+    this.getJson();
+  },
+  getData(jsonContent) {
+    this.charachters = JSON.parse(jsonContent);
+    console.log(this.charachters);
+  },
+  getJson() {
+    const request = new XMLHttpRequest();
+    request.onload = () => {
+      this.getData(request.responseText);
+    };
+    request.open('GET', '/json/got.json');
+    request.send();
+  },
+};
+GameOfThrones.init();
